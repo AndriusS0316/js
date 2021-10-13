75d12d13d3cfd9d55c79de71d18d5170375b0ddc
Andrius <andriuss0316@gmail.com>
2021-10-13 19:35:03 +0300

    20211013

---
+++ abc.js
@@ -0,0 +1,103 @@
+// let a=(new Array("test",1,2,3,-1,-2,-3,0xFFFFF)).sort()
+
+// console.log(a);
+
+// let b=new Array(2,4)
+
+// console.log(b);
+
+// a = new Array(1,2,3);
+// for (i in a){ 
+//             alert("a[i]="+a[i]);
+// }
+
+// console.log(a);
+
+// var o1 = {};
+
+// var o2 = new Object();
+
+// console.log(o1, o2);
+
+// x=1;
+// console.log('x = '+x);
+//   var x;
+
+// for (var x=1; x<5; x++){
+//     console.log(x);
+// }
+
+// var x=0;
+// do{
+//  console.log(x);
+//   }while (x>0)
+
+// var x=2;
+// var y=4;
+// var s="Rezultatas: ";
+//     console.log(s+x+y);
+
+// var i = true ? 1 : false ? 2 : 3;
+
+// console.log(i);
+
+// var i=0;
+//   if (i == false) {
+//     console.log("true");
+//   } else {
+//     console.log("false");
+//     }
+
+
+// for (var i = 1; i < 10; i++) {
+//     if ((i % 2) == 0) {
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+// var i = 0;
+//     var suma = 0;
+//     while (i < 20) {
+//         suma = suma + 1;
+//     }
+//       console.log(suma);
+
+// function print(number) {
+//     if (number != 1) print(number-1);
+//     console.log(number);
+// }
+//   print(5);
+
+
+// function print(number) {
+//     console.log(number);
+//     if (number != 1) print(number-1); 
+// }
+//   print(5);
+
+
+// var a = 100;
+//     var b = a % 3;
+//     var c = Math.floor(a / 3);
+//     var d = c * 3 + b;
+//       console.log(d);
+
+
+// var x = 10;
+//     var y = ++x   +   x++ ;
+//       console.log(y);
+
+
+// var x= 5;
+//     do {
+//         console.log(x);
+//         x++;
+//       } while (x <= 5);
+
+
+var status = "false";
+    if(status)
+        console.log("Active");
+    else
+          console.log("Blocked");
